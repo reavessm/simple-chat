90027144eaeec1b66c0c5e03dd5b91e106ea901d
Stephen Reaves <reaves735@gmail.com>
2024-10-09 22:09:56 -0400

    Handle leaving room
    Signed-off-by: Stephen Reaves <reaves735@gmail.com>

--- chat/src/bin/client.rs
+++ chat/src/bin/client.rs
@@ -3,17 +3,26 @@ pub mod chatter {
 }
 
 use chatter::chat_service_client::ChatServiceClient;
-use chatter::JoinRequest;
+use chatter::{JoinRequest, LeaveRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ChatServiceClient::connect("http://127.0.0.1:8080").await?;
+    let username = String::from("foobar");
 
-    let request = tonic::Request::new(JoinRequest {
-        username: "foobar".into(),
-    });
+    let request = tonic::Request::new(JoinRequest { username });
 
     let response = client.join_room(request).await?;
+
+    println!("Response: {:?}", response);
+
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    let id = &response.get_ref().id;
+
+    let request = tonic::Request::new(LeaveRequest { id: id.to_string() });
+
+    let response = client.leave_room(request).await?;
 
     println!("Response: {:?}", response);
 
--- chat/src/bin/server.rs
+++ chat/src/bin/server.rs
@@ -1,17 +1,24 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use tokio::sync::RwLock;
 use tonic::{transport::Server, Code, Request, Response, Status};
+use uuid::Uuid;
 
 pub mod chatter {
     tonic::include_proto!("chatter");
 }
 
 use chatter::chat_service_server::{ChatService, ChatServiceServer};
-use chatter::{JoinRequest, JoinResponse};
+use chatter::{JoinRequest, JoinResponse, LeaveRequest, LeaveResponse};
+
+#[derive(Debug, Default)]
+pub struct UserMappings {
+    usernames: HashSet<String>,
+    uuid_to_usernames: HashMap<Uuid, String>,
+}
 
 #[derive(Debug, Default)]
 pub struct ChatServer {
-    usernames: RwLock<HashSet<String>>,
+    user_mappings: RwLock<UserMappings>,
 }
 
 #[tonic::async_trait]
@@ -22,16 +29,65 @@ impl ChatService for ChatServer {
     ) -> Result<Response<JoinResponse>, Status> {
         let username = &request.get_ref().username;
 
-        if self.usernames.read().await.contains(username) {
+        if self.user_mappings.read().await.usernames.contains(username) {
             println!("Could not add {username} ...");
             return Err(Status::new(Code::AlreadyExists, "username already exists"));
         }
 
+        let user_id = Uuid::new_v4();
+
         println!("{username} is joining");
 
-        self.usernames.write().await.insert(username.clone());
+        {
+            let mut guard = self.user_mappings.write().await;
 
-        Ok(Response::new(JoinResponse::default()))
+            guard.usernames.insert(username.clone());
+            guard.uuid_to_usernames.insert(user_id, username.clone());
+        }
+
+        Ok(Response::new(JoinResponse { id: user_id.into() }))
+    }
+
+    async fn leave_room(
+        &self,
+        request: Request<LeaveRequest>,
+    ) -> Result<Response<LeaveResponse>, Status> {
+        let id_string = &request.get_ref().id;
+        let id = Uuid::try_parse(id_string);
+
+        if id.is_err() {
+            return Err(Status::invalid_argument(format!(
+                "Invalid uuid: {id_string}"
+            )));
+        }
+
+        let id = id.unwrap();
+
+        if !self
+            .user_mappings
+            .read()
+            .await
+            .uuid_to_usernames
+            .contains_key(&id)
+        {
+            println!("Invalid uuid");
+            return Err(Status::not_found(format!("UUID not found: {id_string}")));
+        }
+
+        {
+            let mut guard = self.user_mappings.write().await;
+
+            let (_, username) = guard
+                .uuid_to_usernames
+                .remove_entry(&id)
+                .expect("UUID Unexpectedly deleted");
+
+            println!("User {username} is leaving ...");
+
+            guard.usernames.remove(&username);
+        }
+
+        Ok(Response::new(LeaveResponse::default()))
     }
 }
 
