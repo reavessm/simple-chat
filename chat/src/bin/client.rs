a95b96fbe9c9a88de0289ce6817c9368fdae13cc
Stephen Reaves <reaves735@gmail.com>
2024-10-09 23:15:18 -0400

    Handle bidirectional messages
    Signed-off-by: Stephen Reaves <reaves735@gmail.com>

--- chat/src/bin/client.rs
+++ chat/src/bin/client.rs
@@ -2,29 +2,159 @@ pub mod chatter {
     tonic::include_proto!("chatter");
 }
 
+use std::io::{stdin, stdout, Write};
+
 use chatter::chat_service_client::ChatServiceClient;
-use chatter::{JoinRequest, LeaveRequest};
+use chatter::{ClientMessage, JoinRequest, LeaveRequest};
+use futures::pin_mut;
+use futures::stream::StreamExt;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ChatServiceClient::connect("http://127.0.0.1:8080").await?;
-    let username = String::from("foobar");
 
-    let request = tonic::Request::new(JoinRequest { username });
+    let mut username = String::new();
+
+    print!("Please enter a username: ");
+    stdout().flush()?;
+    stdin().read_line(&mut username)?;
+
+    let username = username.trim().to_string();
+
+    let request = tonic::Request::new(JoinRequest {
+        username: username.clone(),
+    });
 
     let response = client.join_room(request).await?;
 
-    println!("Response: {:?}", response);
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+    let (exit_tx, mut exit_rx) = tokio::sync::mpsc::channel(1);
+    let (io_tx, mut io_rx) = tokio::sync::mpsc::channel::<String>(10);
+    let io_tx_clone = io_tx.clone();
 
-    std::thread::sleep(std::time::Duration::from_secs(3));
+    let response = std::sync::Arc::new(response);
+    let response_clone = std::sync::Arc::clone(&response);
 
-    let id = &response.get_ref().id;
+    let initial_message_text = "JOINING";
+    let initial_message = ClientMessage {
+        id: response.get_ref().clone().id,
+        username: username.clone(),
+        message: initial_message_text.to_string(),
+    };
 
-    let request = tonic::Request::new(LeaveRequest { id: id.to_string() });
+    if tx.send(initial_message).is_err() {
+        let err_msg = "Failed to send initial message";
+        eprintln!("{err_msg}");
+        return Err(err_msg.into());
+    }
 
-    let response = client.leave_room(request).await?;
+    let print_handle = tokio::spawn(async move {
+        let mut stdout = tokio::io::stdout();
+        while let Some(message) = io_rx.recv().await {
+            let _ = stdout.write_all(message.as_bytes()).await;
+            let _ = stdout.flush().await;
+        }
+    });
 
-    println!("Response: {:?}", response);
+    let input_handle = tokio::spawn(async move {
+        let id = &response_clone.get_ref().id;
+        let stdin = tokio::io::stdin();
+        let mut stdin_reader = tokio::io::BufReader::new(stdin).lines();
+
+        let prompt = "> ";
+        let _ = io_tx.send(prompt.into()).await;
+
+        while let Ok(Some(line)) = stdin_reader.next_line().await {
+            let message = line.trim().to_string();
+
+            if message == "exit" {
+                let _ = exit_tx.send(());
+                return;
+            }
+
+            let message_request = ClientMessage {
+                message,
+                id: id.clone(),
+                username: username.clone(),
+            };
+
+            if tx.send(message_request).is_err() {
+                eprintln!("Failed to send message");
+                break;
+            }
+
+            let _ = io_tx.send(prompt.into()).await;
+        }
+    });
+
+    let outbound_stream = Box::pin(tokio_stream::wrappers::UnboundedReceiverStream::new(rx));
+
+    let response_stream = client
+        .message(tonic::Request::new(outbound_stream))
+        .await?
+        .into_inner();
+
+    let reply_handle = tokio::spawn(async move {
+        pin_mut!(response_stream);
+
+        loop {
+            tokio::select! {
+                biased;
+                Some(response) = response_stream.next() => {
+                    match response {
+                        Ok(resp) => {
+                            let _ = io_tx_clone.send(format!("\r{:>.10}> {}\n> ", resp.username, resp.message)).await;
+                        },
+                        Err(e) => {
+                            eprintln!("ERROR! {:?}", e);
+                            break;
+                        }
+                    }
+                },
+                    _ = exit_rx.recv() => break,
+            }
+        }
+    });
+
+    let _ = futures::join!(reply_handle, input_handle, print_handle);
+
+    let request = tonic::Request::new(LeaveRequest {
+        id: response.get_ref().id.to_string(),
+    });
+
+    let _ = client.leave_room(request).await?;
 
     Ok(())
 }
+
+// async fn handle_message(username: String, id: String) -> impl Stream<Item = ClientMessage> {
+//     async_stream::stream! {
+//         // loop {
+//             let username = username.clone();
+//             let id = id.clone();
+//
+//             print!("> ");
+//             stdout().flush()?;
+//
+//             let mut message = String::new();
+//             stdin().read_line(&mut message)?;
+//             let message = message.trim().to_string();
+//
+//             if message == "exit" {
+//                 yield Err(Status::new(Code::Unknown, ""));
+//                 return;
+//             } //else {
+//
+//                 yield Ok(ClientMessage{ id, username, message });
+//             // }
+//
+//         // }
+//     }
+//     .filter_map(|item| async {
+//         match item {
+//             Ok(value) => Some(value),
+//             Err(_) => None,
+//         }
+//     })
+// }
--- chat/src/bin/server.rs
+++ chat/src/bin/server.rs
@@ -1,6 +1,12 @@
+use futures::future;
 use std::collections::{HashMap, HashSet};
+use std::pin::Pin;
+use std::sync::Arc;
+use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::RwLock;
-use tonic::{transport::Server, Code, Request, Response, Status};
+use tokio::task::JoinHandle;
+use tokio_stream::Stream;
+use tonic::{transport::Server, Code, Request, Response, Status, Streaming};
 use uuid::Uuid;
 
 pub mod chatter {
@@ -8,7 +14,9 @@ pub mod chatter {
 }
 
 use chatter::chat_service_server::{ChatService, ChatServiceServer};
-use chatter::{JoinRequest, JoinResponse, LeaveRequest, LeaveResponse};
+use chatter::{
+    ClientMessage, JoinRequest, JoinResponse, LeaveRequest, LeaveResponse, ServerMessage,
+};
 
 #[derive(Debug, Default)]
 pub struct UserMappings {
@@ -16,9 +24,11 @@ pub struct UserMappings {
     uuid_to_usernames: HashMap<Uuid, String>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ChatServer {
     user_mappings: RwLock<UserMappings>,
+    user_streams: Arc<RwLock<HashMap<Uuid, Sender<Result<ServerMessage, Status>>>>>,
+    messages_sender: Sender<(Uuid, ServerMessage)>,
 }
 
 #[tonic::async_trait]
@@ -28,9 +38,17 @@ impl ChatService for ChatServer {
         request: Request<JoinRequest>,
     ) -> Result<Response<JoinResponse>, Status> {
         let username = &request.get_ref().username;
+        let mut username = username.to_string();
+        username.truncate(10);
 
-        if self.user_mappings.read().await.usernames.contains(username) {
-            println!("Could not add {username} ...");
+        if self
+            .user_mappings
+            .read()
+            .await
+            .usernames
+            .contains(&username)
+        {
+            eprintln!("Could not add {username} ...");
             return Err(Status::new(Code::AlreadyExists, "username already exists"));
         }
 
@@ -63,38 +81,180 @@ impl ChatService for ChatServer {
 
         let id = id.unwrap();
 
+        if let Err(reason) = self.remove_user(id).await {
+            return Err(reason);
+        }
+
+        Ok(Response::new(LeaveResponse::default()))
+    }
+
+    type MessageStream =
+        Pin<Box<dyn Stream<Item = Result<ServerMessage, Status>> + Send + 'static>>;
+
+    async fn message(
+        &self,
+        request: Request<Streaming<ClientMessage>>,
+    ) -> Result<Response<Self::MessageStream>, Status> {
+        let mut stream = request.into_inner();
+
+        let Ok(Some(msg)) = stream.message().await else {
+            return Err(Status::unknown("Unkown error"));
+        };
+
+        let (username, id, message) = {
+            let guard = self.user_mappings.read().await;
+
+            let id = Uuid::try_parse(&msg.id);
+
+            if id.is_err() {
+                return Err(Status::invalid_argument(format!(
+                    "Invalid uuid: {}",
+                    msg.id
+                )));
+            }
+
+            let id = id.unwrap();
+
+            let username = guard.uuid_to_usernames.get(&id);
+
+            if username.is_none() {
+                return Err(Status::not_found(format!("UUID not found: {}", msg.id)));
+            }
+
+            (username.unwrap().clone(), id, msg.message)
+        };
+
+        let (tx, mut rx) = tokio::sync::mpsc::channel(10);
+
+        let timestamp = chrono::Utc::now().to_rfc3339();
+
+        println!("{} {}: {}", timestamp.clone(), username, message);
+
+        let output_stream = async_stream::stream! {
+            while let Some(msg) = rx.recv().await {
+                yield msg;
+            }
+        };
+
+        // TODO: Add stream to server
+
+        // {
+        //     // let mut guard = self.user_streams.write().await;
+        //     //
+        //     // guard.insert(id, tx.into());
+        // }
+        self.user_streams.write().await.insert(id, tx.into());
+
+        let sender = self.messages_sender.clone();
+
+        let _ = tokio::spawn(async move {
+            while let Ok(msg) = stream.message().await {
+                println!("Looping");
+                if msg.is_none() {
+                    println!("no msg");
+                    break;
+                }
+
+                let timestamp = chrono::Utc::now().to_rfc3339();
+
+                let message = msg.unwrap().message;
+                let username = username.clone();
+
+                println!("{} {}: {}", timestamp.clone(), username, message);
+
+                let relay_result = sender
+                    .send((
+                        id,
+                        ServerMessage {
+                            username,
+                            message,
+                            timestamp,
+                        },
+                    ))
+                    .await;
+                if relay_result.is_err() {
+                    eprintln!("ERROR: {:?}", relay_result);
+                }
+            }
+        });
+
+        Ok(Response::new(Box::pin(output_stream) as Self::MessageStream))
+    }
+}
+
+impl ChatServer {
+    async fn remove_user(&self, uuid: Uuid) -> Result<(), Status> {
+        println!("Removing");
         if !self
             .user_mappings
             .read()
             .await
             .uuid_to_usernames
-            .contains_key(&id)
+            .contains_key(&uuid)
         {
-            println!("Invalid uuid");
-            return Err(Status::not_found(format!("UUID not found: {id_string}")));
+            println!("Id not found");
+            return Err(Status::not_found(format!("UUID not found: {uuid}")));
         }
 
+        let mut guard = self.user_mappings.write().await;
+        let mut stream_guard = self.user_streams.write().await;
+
+        let (_, username) = guard
+            .uuid_to_usernames
+            .remove_entry(&uuid)
+            .expect("UUID Unexpectedly deleted");
+
+        println!("User {username} is leaving ...");
+
+        guard.usernames.remove(&username);
+        stream_guard.remove_entry(&uuid);
+
+        println!("User {username} has left.");
+
+        Ok(())
+    }
+}
+
+async fn relay(
+    mut server_receiver: Receiver<(Uuid, ServerMessage)>,
+    client_streams: Arc<RwLock<HashMap<Uuid, Sender<Result<ServerMessage, Status>>>>>,
+) {
+    while let Some((sender_id, msg)) = server_receiver.recv().await {
         {
-            let mut guard = self.user_mappings.write().await;
+            let guard = client_streams.read().await;
 
-            let (_, username) = guard
-                .uuid_to_usernames
-                .remove_entry(&id)
-                .expect("UUID Unexpectedly deleted");
+            let mut handles: Vec<JoinHandle<()>> = Vec::with_capacity(guard.len());
 
-            println!("User {username} is leaving ...");
+            for (_uuid, sender) in guard.iter().filter(|(&u, _)| u != sender_id) {
+                let message = msg.clone();
+                let sender = sender.clone();
 
-            guard.usernames.remove(&username);
+                let handle = tokio::spawn(async move {
+                    let _result = sender.send(Ok(message)).await;
+                });
+
+                handles.push(handle);
+            }
+            let _ = future::join_all(handles).await;
         }
-
-        Ok(Response::new(LeaveResponse::default()))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:8080".parse()?;
-    let server = ChatServer::default();
+    let (tx, rx) = tokio::sync::mpsc::channel(10);
+    let server = ChatServer {
+        messages_sender: tx,
+        user_streams: Default::default(),
+        user_mappings: Default::default(),
+    };
+
+    let client_streams = server.user_streams.clone();
+
+    let server_handle = tokio::spawn(async move {
+        relay(rx, client_streams).await;
+    });
 
     println!("Listening on {addr} ...");
 
@@ -102,6 +262,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_service(ChatServiceServer::new(server))
         .serve(addr)
         .await?;
+
+    let _ = server_handle.await;
 
     Ok(())
 }
